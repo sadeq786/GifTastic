99102bb89665abeccf9814a177d1c6b2aca9e9cb
Shaikh Rahman <shaikh.s.rahman@gmail.com>
2018-04-30 21:52:02 -0500

    ADDED SIDEBAR

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,92 @@
 $(document).ready(function () {
+    // Creating an array that holds all the categories of gifs that I want to display
     var topics = [
         "cats",
         "birds",
         "fish",
         "deer",
-        "lions"
+        "lions",
+        "beaver",
+        "rabbit",
+        "octopus"
     ];
+    // get input from textbox, save that information and append topics[i].
+    $("#add-animal").on("click", function (event) {
+        event.preventDefault();
+        var newAnimal = $("#animal").val();
+        console.log("newAnimal: " + newAnimal);
+        topics.push(newAnimal);
+    });
+    
     for (var i = 0; i < topics.length; i++) {
+        // Creating one button at a time
         var button = $("<button>");
+        // Adding classes to the button we just created
         button.addClass("btn btn-info");
+        // Adding text/label to the button we just created
         button.text(topics[i]);
-        // console.log(this);
+        // Once a button is clicked, the following code executes
         button.on("click", function () {
-
+            // First things first, it empties the div of previously loaded gifs
             $("#gifs").empty();
-            ;            // console.log(this);
+            // buttonText is being created to add it as a variable at the end of the URL
             var buttonText = $(this).text();
             // console.log(buttonText);
             var API_KEY = "AqC8LA9EVbLuMtO5IJ87fgPSKFh67ybW";
             var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&limit=10&q=" + buttonText;
             console.log(requestUrl);
-
             $.ajax({
                 method: "GET",
                 url: requestUrl
             }).then(function (response) {
-                // code goes here  
-                // create the element
+                console.log(response);
                 for (var i = 0; i < response.data.length; i++) {
-                    var img = $("<img>");
-                    // set the src of the element
-                    img.attr("src", response.data[i].images.downsized.url);
-                    img.attr("data-animated", response.data[i].images.downsized.url);
-                    img.attr("data-still", response.data[i].images.downsized_still.url);
-                    img.attr("data-state", "animated");
-
-                    img.on("click", function () {
-                        console.log(this);
-                        var state = $(this).attr("data-state");
-                        console.log(state);
-                        // If data-state is animated, change to still. 
-                        if (state === "animated") {
-                            $(this).attr("data-state", "still");
-                            console.log("if is working");
-                            console.log();
-                            // Change the src of the animated to still version
-                            $(this).attr("src", $(this).attr("data-still"));
-                        } else {
-                            $(this).attr("data-state", "animated");
-                            console.log("else is working");
-                            // else change the state to animated and change src to still version
-                            $(this).attr("src", $(this).attr("data-animated"));
-                        }
-                    });
-                    // attach the element to the body
+                    var img = createImage(response, i);
                     $("#gifs").append(img);
                 }
             });
-
         });
         $("#buttons").append(button);
-
     }
+
+    function createImage(response, i) {
+        var img = $("<img>");
+        img.addClass("giphy");
+        // set the src of the element
+        img.attr("src", response.data[i].images.downsized.url);
+        img.attr("data-animated", response.data[i].images.downsized.url);
+        img.attr("data-still", response.data[i].images.downsized_still.url);
+        img.attr("data-state", "animated");
+
+        var gifCard = $("<div class='gif-card'>");
+        var pRating = $("<p>");
+
+        gifCard.append(img);
+        pRating.append("Rating: " + response.data[i].rating);
+        gifCard.append(pRating);
+        console.log("gifCard = " + gifCard)
+        return gifCard;
+    }
+
+    // Clicking on amy image will trigger this function
+    $(document).on("click", ".giphy", function () {
+        console.log("image: " + this);
+        var state = $(this).attr("data-state");
+        console.log(state);
+        // If data-state is animated, change to still. 
+        if (state === "animated") {
+            $(this).attr("data-state", "still");
+            console.log("if statement is working");
+            // Change the src of the animated to still version
+            $(this).attr("src", $(this).attr("data-still"));
+        } else {
+            $(this).attr("data-state", "animated");
+            console.log("else statement is working");
+            // else change the state to animated and change src to still version
+            $(this).attr("src", $(this).attr("data-animated"));
+        }
+    });
 });
+ 
+
+
